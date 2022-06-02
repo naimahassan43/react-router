b8633730bf4486d86f958ead5250fbad42327831
Naima Hassan <naima.hassan434@gmail.com>
2022-06-02 10:11:44 +0100

    index pages

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import About from "./pages/About";
 import Error from "./pages/Error";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+import SharedLayout from "./pages/SharedLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}>
+        <Route path="/" element={<SharedLayout/>}>
+          <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/products" element={<Products />} />
           <Route path="*" element={<Error />} />
