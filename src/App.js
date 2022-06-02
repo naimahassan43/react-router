ee53a1ae007dfd0f20bf6ff791e88d1eafdb783d
Naima Hassan <naima.hassan434@gmail.com>
2022-06-02 02:00:40 +0100

    Page components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import About from './pages/About';
+import Home from './pages/Home';
+import Products from './pages/Products';
+
 function App() {
-  return <h2>React Router 6 Tutorial</h2>;
+  return <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/about' element={<About/>} />
+    <Route path='/products' element={<Products/>} /> 
+  </Routes>
+  </BrowserRouter>;
 }
 
 export default App;
