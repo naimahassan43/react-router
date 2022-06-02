c24fb400bb299a84502695949a8c3b8d6a076ada
Naima Hassan <naima.hassan434@gmail.com>
2022-06-02 11:41:23 +0100

    Prodauct page refactor

--- src/App.js
+++ src/App.js
@@ -9,23 +9,30 @@ import SingleProduct from "./pages/SingleProduct";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import ProtectedRoute from "./pages/ProtectedRoute";
+import SharedProductLayout from "./pages/SharedProductLayout";
 
 function App() {
-  const [user, setUser]=useState()
+  const [user, setUser] = useState();
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="products" element={<Products />} />
-          <Route path="products/:productId" element={<SingleProduct />} />
+          <Route path="products" element={<SharedProductLayout />}>
+            <Route index element={<Products />} />
+            <Route path=":productId" element={<SingleProduct />} />
+          </Route>
+
           <Route path="login" element={<Login setUser={setUser} />} />
-          <Route path="dashboard" element={
-          <ProtectedRoute user={user}>
-            <Dashboard user={user} />
-          </ProtectedRoute>
-          } />
+          <Route
+            path="dashboard"
+            element={
+              <ProtectedRoute user={user}>
+                <Dashboard user={user} />
+              </ProtectedRoute>
+            }
+          />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
